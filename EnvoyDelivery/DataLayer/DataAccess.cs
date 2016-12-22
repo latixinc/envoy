eb888f38d0142950db3bf1b1a9bc6db539acee14
latixinc <latixinc@gmail.com>
2016-12-22 16:36:48 +0100

    Add files via upload

---
+++ EnvoyDelivery/DataLayer/DataAccess.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvoyDelivery.DataLayer
+{
+    class DataAccess
+    {
+
+        public static string ConnectionString
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["EnvoyData"].ConnectionString.ToString();
+            }
+        }
+        public static SqlParameter AddParameter(string parameterName, object value, SqlDbType DbType, int size)
+        {
+            SqlParameter param = new SqlParameter();
+            param.ParameterName = parameterName;
+            param.Value = value;
+            param.SqlDbType = DbType;
+            param.Size = size;
+            param.Direction = ParameterDirection.Input;
+            return param;
+        }
+        public static DataTable ExecuteDTByProcedure(string ProcedureName, SqlParameter[] Params)
+        {
+            SqlConnection conn = new SqlConnection(ConnectionString);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = ProcedureName;
+            cmd.Parameters.AddRange(Params);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adopter = new SqlDataAdapter(cmd);
+            DataTable dTable = new DataTable();
+
+            try
+            {
+                adopter.Fill(dTable);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //Disposing Objects
+                adopter.Dispose();
+                cmd.Parameters.Clear();
+                cmd.Dispose();
+                conn.Dispose();
+            }
+            return dTable;
+        }
+      
+    }
+}
